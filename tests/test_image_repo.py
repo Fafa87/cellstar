ff9f514eee877706a7f383bd59e3b3a745193025
Filip Mróz <fafafft@gmail.com>
2022-11-07 09:48:40 +0100

    save intermediate visuals for test

--- tests/test_image_repo.py
+++ tests/test_image_repo.py
@@ -7,6 +7,7 @@ import numpy.testing as nptest
 
 import cellstar.utils.params_util
 import cellstar.core.image_repo
+import cellstar.utils.debug_util
 
 TESTS = os.path.dirname(__file__)
 
@@ -20,16 +21,33 @@ def get_expected(sample_file, name):
     return os.path.join(TESTS, "expected", sample_name, name + ".tif")
 
 
+def prepare_output_dir(sample_file):
+    sample_name = os.path.splitext(sample_file)[0]
+    output_dir = os.path.join(TESTS, "output", sample_name)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    return output_dir
+
 class TestImageRepo(unittest.TestCase):
+    def setUp(self):
+        self.debug_image_path = cellstar.utils.debug_util.debug_image_path
+
     def test_on_example_image(self):
+        output_dir = prepare_output_dir("sample_brightfield")
+        cellstar.utils.debug_util.debug_image_path = output_dir
+
         input_image = imageio.imread(get_input("sample_brightfield.tif"))
 
         parameters = cellstar.utils.params_util.default_parameters(9, 35)
         images = cellstar.core.image_repo.ImageRepo(input_image, parameters)
+        cellstar.utils.debug_util.images_repo_save(images)
 
         expected_darker = imageio.imread(get_expected("sample_brightfield.tif", "darker"))
         nptest.assert_almost_equal(expected_darker, images.darker, decimal=2)
         expected_cell_content_mask = imageio.imread(get_expected("sample_brightfield.tif", "cell_content_mask"))
         expected_cell_content_mask = expected_cell_content_mask > 0
         differences = expected_cell_content_mask != images.cell_content_mask
-        assert differences.mean() < 0.0001
+        assert differences.mean() < 0.0001, differences.mean()
+
+    def tearDown(self):
+        cellstar.utils.debug_util.debug_image_path = self.debug_image_path
